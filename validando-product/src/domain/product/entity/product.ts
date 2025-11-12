98ee10e6abe09b50429bc59dc46d329ef1f7069e
jmmenezes <joao.marcosmenezes@portoseguro.com.br>
2025-11-12 21:15:56 +0000

    Validator para product

--- validando-product/src/domain/product/entity/product.ts
+++ validando-product/src/domain/product/entity/product.ts
@@ -1,4 +1,5 @@
 import Entity from "../../@shared/entity/entity.abstract";
+import ProductValidatorFactory from "../factory/product.validator.factory";
 import ProductInterface from "./product.interface";
 
 export default class Product extends Entity implements ProductInterface {
@@ -37,20 +38,7 @@ export default class Product extends Entity implements ProductInterface {
   }
 
   validate() {
-    if (this._id.length === 0) {
-      this.notification.addError({
-          context: "product",
-          message: "Id is required"});
-    }
-    if (this._name.length === 0) {
-      this.notification.addError({
-          context: "product",
-          message: "Name is required"});
-    }
-    if (this._price < 0) {
-      this.notification.addError({
-        context: "product",
-        message: "Price must be greater than zero"});
-    }
+    ProductValidatorFactory.create().validate(this);
   }
+
 }
