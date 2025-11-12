2af6ae29162c1f489c39c3dbadf24a242d3c3a07
jmmenezes <joao.marcosmenezes@portoseguro.com.br>
2025-11-12 20:55:16 +0000

    Desafio notification em product

--- notification/src/domain/product/entity/product.spec.ts
+++ notification/src/domain/product/entity/product.spec.ts
@@ -2,21 +2,21 @@ import Product from "./product";
 
 describe("Product unit tests", () => {
   it("should throw error when id is empty", () => {
-    expect(() => {
-      const product = new Product("", "Product 1", 100);
-    }).toThrowError("Id is required");
+    const product = new Product("", "Product 1", 100);
+    expect(product.notification.hasErrors()).toBe(true);
+    expect(product.notification.messages()).toBe("product: Id is required,");
   });
 
   it("should throw error when name is empty", () => {
-    expect(() => {
-      const product = new Product("123", "", 100);
-    }).toThrowError("Name is required");
+    const product = new Product("123", "", 100);
+    expect(product.notification.hasErrors()).toBe(true);
+    expect(product.notification.messages()).toBe("product: Name is required,");
   });
 
   it("should throw error when price is less than zero", () => {
-    expect(() => {
-      const product = new Product("123", "Name", -1);
-    }).toThrowError("Price must be greater than zero");
+    const product = new Product("123", "Name", -1);
+    expect(product.notification.hasErrors()).toBe(true);
+    expect(product.notification.messages()).toBe("product: Price must be greater than zero,");
   });
 
   it("should change name", () => {
