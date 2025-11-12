0716c2cbdb784aea9300f28357a1a3eeed7b6d9b
jmmenezes <joao.marcosmenezes@portoseguro.com.br>
2025-11-12 20:57:11 +0000

    Adicione um teste que acumule dois erros ao mesmo tempo.

--- notification/src/domain/product/entity/product.spec.ts
+++ notification/src/domain/product/entity/product.spec.ts
@@ -19,6 +19,12 @@ describe("Product unit tests", () => {
     expect(product.notification.messages()).toBe("product: Price must be greater than zero,");
   });
 
+  it("should throw error when id and name is empty", () => {
+    const product = new Product("", "", 100);
+    expect(product.notification.hasErrors()).toBe(true);
+    expect(product.notification.messages()).toBe("product: Id is required,product: Name is required,");
+  });
+
   it("should change name", () => {
     const product = new Product("123", "Product 1", 100);
     product.changeName("Product 2");
